432c553c12a06534bc17a16dfad23ae1f31f990a
omin-Kwon <om0127@snu.ac.kr>
2024-05-25 13:50:02 +0900

    KnowledgeGraphListView, KnowledgeGraphDetailView

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views_for_api import ExtractConceptsView, KnowledgeGraphView
+from .views_for_api import ExtractConceptsView, KnowledgeGraphView, KnowledgeGraphChatListView, KnowledgeGraphChatDetailView
 from .views import SuperConceptView
 from .views_legacy import IncrementView
 
@@ -7,5 +7,7 @@ urlpatterns = [
     path('increment/', IncrementView.as_view(), name='increment'),
     path('chat/', ExtractConceptsView.as_view(), name='chat'),
     path('graph/', KnowledgeGraphView.as_view(), name='graph'),
-    path('superconcept/', SuperConceptView.as_view(), name='super_concept')
+    path('superconcept/', SuperConceptView.as_view(), name='super_concept'),
+    path('graph_chat_list/', KnowledgeGraphChatListView.as_view(), name='graph_chat'),
+    path('graph_chat_detail/', KnowledgeGraphChatDetailView.as_view(), name='graph_chat_detail')
 ]
